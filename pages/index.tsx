8d0a437ff1ba69cd9b161d73ecd2302317eeb4fb
Ester Gama <esterfaniasantos@gmail.com>
2021-06-04 18:03:31 -0300

    feat:create AuthContext

---
+++ pages/index.tsx
@@ -0,0 +1,36 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import { FormEvent, useContext, useState } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { signIn, isAuthenticated } = useContext(AuthContext);
+
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    const data = {
+      email,
+      password,
+    };
+    await signIn(data);
+  }
+  return (
+    <form className={styles.container} onSubmit={handleSubmit}>
+      <input
+        type='email'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type='password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type='submit'>Entrar</button>
+    </form>
+  );
+}
