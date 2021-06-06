22d3b14afd7ecb46e2356d739ed91d2ea3f9e3dc
Ester Gama <esterfaniasantos@gmail.com>
2021-06-06 20:30:09 -0300

    (feat): set token

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, ReactNode, useEffect, useState } from 'react';
 import Router from 'next/router';
 import { api } from '../services/api';
-import { setCookie } from 'nookies';
+import { setCookie, parseCookies } from 'nookies';
 
 type User = {
   email: string;
@@ -30,6 +30,16 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
 
+  useEffect(() => {
+    const { 'auth.token': token } = parseCookies();
+    if (token) {
+      api.get('/me').then((res) => {
+        const { email, permissions, roles } = res.data;
+        setUser({ email, permissions, roles });
+      });
+    }
+  }, []);
+
   async function signIn({ email, password }: SignInCredentials) {
     try {
       const response = await api.post('sessions', {
@@ -42,6 +52,8 @@ export function AuthProvider({ children }: AuthProviderProps) {
       setTokens({ token, refreshToken });
 
       setUser({ email, permissions, roles });
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`;
 
       Router.push('/dashboard');
     } catch (err) {
